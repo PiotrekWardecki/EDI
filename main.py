aab1f204a6c9231ac66e39ffbb813327dcde9267
Mateusz <maciaszek96@gmail.com>
2020-11-14 18:44:54 +0100

    add session analysis

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from datetime import datetime
 import shlex
 import pandas as pd
+import arff
 
-
-
-TIME_THRESHOLD = 30 #minutes
-# infile = open('res/Aug28_log','rb')
+TIME_THRESHOLD = 30*60 #30minutes in seconds
+# infile = open('res/Aug28_log.txt','r')
 infile = open('res/logfile.txt', 'r')
-outfile = open('res/logfile.csv', 'w')
+# outfile = open('res/logfile.csv', 'w')
 
 count = 1
-extension_set = {"gif", "jpg", "jpeg","bmp", "xbm", "GIF", "JPG"}
+extension_set = {"gif", "jpg", "jpeg","bmp", "xbm", "GIF", "JPG", 'wav'}
 user_set = {None}
 websites = {}
 df = pd.DataFrame()
+last_user_visit = {}
+last_user_session_id = {}
+next_session_id = 0
 
-
+start_datetime = None
 for line in infile.readlines():
     # print(line)
     try:
@@ -27,14 +29,18 @@ for line in infile.readlines():
     out_list = []
 
     # 1: client
-    out_list.append(lineaslist[0])
-    user_set.add(lineaslist[0])
+    user = lineaslist[0]
+    out_list.append(user)
+    user_set.add(user) #todo: delete
 
     raw_timestamp = lineaslist[3]
-    timestamp = datetime.strptime(raw_timestamp, '[%d/%b/%Y:%H:%M:%S') #'%b %d %Y %I:%M%p')
+    time_and_date = datetime.strptime(raw_timestamp, '[%d/%b/%Y:%H:%M:%S') #'%b %d %Y %I:%M%p')
+    if start_datetime is None:
+        start_datetime = time_and_date
+
     # 2, 3: date, time
-    out_list.append(str(timestamp.date()))
-    out_list.append(str(timestamp.time()))
+    out_list.append(str(time_and_date.date()))
+    out_list.append(str(time_and_date.time()))
 
     method_req_protocol = lineaslist[5].split()
     if len(method_req_protocol)==2:
@@ -93,34 +99,93 @@ for line in infile.readlines():
     #     else:
     #         out_list[i] = str(el)
 
+    # series = pd.Series(out_list)
+    timestamp = (time_and_date-start_datetime).total_seconds()
+
+    if user not in last_user_session_id:
+        last_user_session_id[user] = next_session_id
+        last_user_visit[user] = timestamp
+        next_session_id+=1
+    else:
+        timebreak = timestamp-last_user_visit[user]
+        if timebreak>TIME_THRESHOLD:
+            last_user_session_id[user]=next_session_id
+            next_session_id+=1
+    current_session_id = last_user_session_id[user]
+    last_user_visit[user] = timestamp
+
+
+    series = pd.Series([user, req, timestamp, current_session_id])
+    df = df.append(series, ignore_index=True)
+
     for i, el in enumerate(out_list):
         out_list[i] = str(el)
 
-    series = pd.Series(out_list)
-    df = df.append(series, ignore_index=True)
-
     out_str = ",".join(out_list)
 
-    outfile.write(out_str)
-    outfile.write('\n')
+    # outfile.write(out_str)
+    # outfile.write('\n')
     count+=1
-    if count>100:
+    if count>300:
         break
+
 print(len(user_set))
 infile.close()
-outfile.close()
+# outfile.close()
 
-websites_count = len(websites)
-for website, occur in websites.items():
-    if occur>0.005*50000:
-        print(website, occur)
+# websites_count = len(websites)
+# for website, occur in websites.items():
+#     if occur>0.005*50000:
+#         print(website, occur)
 
 
 
-print(websites_count)
+# print(websites_count)
 print(df)
 
-websites_and_visits = df[4].value_counts()
+#how many times each website was visited
+websites_and_visits = df[1].value_counts()
+#how many visits in general
+total_visits = websites_and_visits.sum()
+#choose the most often visited sites
+websites_and_visits = websites_and_visits[websites_and_visits > 0.005 * total_visits]
+print(len(websites_and_visits))
 
-for i, row in df.iterrows():
-    pass
+# for i, row in df.iterrows():
+#     print(row)
+
+flagged_websites_names = websites_and_visits.index.values.tolist()
+session_cols = ['session_time', 'actions', 'avg_time_per_site'] + flagged_websites_names
+
+session_df= pd.DataFrame(columns=session_cols)
+
+for session in range(0,next_session_id):
+    indexes_of_session = df[df[3]==session].index.values
+    print(indexes_of_session)
+    session_start = df.iloc[indexes_of_session[0]][2]
+    session_end = df.iloc[indexes_of_session[-1]][2]
+    session_time = int(session_end - session_start)
+    if session_time == 0:
+        continue
+    actions = len(indexes_of_session)
+    avg_time_per_site = int( session_time / (actions-1))
+
+    flagged_websites = [False] * len(flagged_websites_names)
+    for index in indexes_of_session:
+        print(df.iloc[index])
+        website_name=df.iloc[index][1]
+        if website_name in flagged_websites_names:
+            website_to_flag_index = flagged_websites_names.index(website_name)
+            flagged_websites[website_to_flag_index] = True
+
+    row = [session_time, actions, avg_time_per_site] + flagged_websites
+    series = pd.Series(row, index=session_cols)
+    print(series)
+    session_df = session_df.append(series, ignore_index=True)
+
+
+print(session_df)
+# print(df)
+
+arff.dump('res/session.arff', session_df.values,
+          relation='session', names=session_df.columns)
